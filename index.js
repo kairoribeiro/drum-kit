8a5944070254585c1175d9e85ba43ae62c054465
Kairo Ribeiro <kairo10@gmail.com>
2022-09-15 23:20:56 -0400

    add switch sounds

--- index.js
+++ index.js
@@ -2,8 +2,41 @@
 let numberOfDrumButtons = document.querySelectorAll(".drum").length
 
 function handleClick() {
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
+    let buttonClicked = this.innerHTML;
+    switch (buttonClicked) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+                var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+                    var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+    
+        default:
+            break;
+    }
+    
 }
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
